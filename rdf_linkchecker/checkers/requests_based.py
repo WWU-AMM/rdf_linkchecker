1ed609fb73c62124c834919317fd4b7ce8ecb7a9
René Fritze <rene.fritze@wwu.de>
2022-01-25 09:42:18 +0100

    send user agent+related headers in requests

--- rdf_linkchecker/checkers/requests_based.py
+++ rdf_linkchecker/checkers/requests_based.py
@@ -5,8 +5,22 @@ import operator
 from pathlib import Path
 
 import requests
+from user_agent import generate_user_agent
 
 from rdf_linkchecker.checkers import CONFIG_DEFAULTS
+
+_SESSION_HEADER = {
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-User": "?1",
+    "Sec-Fetch-Dest": "document",
+    "Referer": "https://www.google.com/",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+}
 
 
 class Checker:
@@ -22,6 +36,9 @@ class Checker:
             self.skip_domains = self.config["skip"]["domains"].split(",")
         except AttributeError:
             self.skip_domains = []
+        self._session = requests.Session()
+        self._session.headers.update(_SESSION_HEADER)
+        self._session.headers.update({"User-Agent": generate_user_agent()})
 
     def _accept_url(self, url):
         for skip in self.skip_domains:
@@ -42,7 +59,7 @@ class Checker:
 
         def _check():
             try:
-                with requests.get(
+                with self._session.get(
                     url, timeout=int(con["timeout"]), stream=True
                 ) as response:
                     try:
