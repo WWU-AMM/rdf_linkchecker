c3e62283dc28d87a8f0d07f20b11de7924211d3b
René Fritze <rene.fritze@wwu.de>
2022-01-25 14:19:44 +0100

    [ci] increase default timeout for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,15 @@ import pytest
 @pytest.fixture()
 def ttl_files(shared_datadir):
     return list(shared_datadir.glob("*.ttl"))
+
+
+@pytest.fixture(autouse=True)
+def increase_timeout(monkeypatch):
+    """Remove requests.sessions.Session.request for all tests."""
+    import rdf_linkchecker
+
+    monkeypatch.setitem(
+        rdf_linkchecker.checkers.CONFIG_DEFAULTS,
+        "connection",
+        {"timeout": 6, "retries": 1},
+    )
